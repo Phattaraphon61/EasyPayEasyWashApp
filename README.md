6eaad13282f223f0b9415d2e66cbff9da46bb5c3
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2022-05-12 12:22:40 +0700

    login

---
+++ README.md
@@ -0,0 +1,21 @@
+# Facebook Flutter Login
+
+Facebook login with Flutter.
+
+<img src=".docs/demo.gif" height=300/>
+
+### Running it
+
+You'll need Flutter 2 SDK and an Android Emulator running.
+
+```bash
+flutter pub get
+flutter run
+```
+
+### Sources
+
+```
+https://facebook.meedu.app/docs/4.x.x/android/
+https://developers.facebook.com/docs/facebook-login/android?locale=en_US
+```
---
+++ lib/splash/splash.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: avoid_print, unnecessary_brace_in_string_interps
+
+import 'package:easypayeasywash/home/home.dart';
+import 'package:easypayeasywash/login/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 4), () async {
+      FacebookPermissions? permissions =
+          await FacebookAuth.instance.permissions;
+      print("uidddd...${permissions}..........");
+      if (permissions != null) {
+        var userData = await FacebookAuth.i.getUserData(
+          fields: "name,email,picture.width(200)",
+        );
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => Home(userData: userData)),
+            (route) => false);
+      }
+      if (permissions == null) {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const Login()),
+            (route) => false);
+      }
+    });
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Image.asset('assets/images/logo.png'),
+      ),
+    );
+  }
+}
